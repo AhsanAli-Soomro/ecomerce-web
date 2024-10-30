57ee75fa49a14970a76c57849e8706412c7711cc
Ahsan Ali Soomro <149801757+AhsanAli-Soomro@users.noreply.github.com>
2024-10-30 13:28:01 +0500

    add comments

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <div>
             <div className="bg-gradient-to-r from-yellow-500 to-yellow-600 py-2">
                 <div className="container mx-auto flex justify-center space-x-4 items-center text-white">
-                Copyright © 2024 - All Rights Reserved. A Product of Ahsan Ali Soomro.
+                    Copyright © 2024 - All Rights Reserved. A Product of Ahsan Ali.
                 </div>
             </div>
         </div>
